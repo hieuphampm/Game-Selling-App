6f2a2fa0ec36c8991cca05a22e47946d03598523
Phuoc Hieu <hieuphamminh976@gmail.com>
2025-07-09 07:56:53 +0700

    Update main.dart

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+
 import 'auth/login.dart';
-import 'screens/home_screen.dart';
 import 'auth/register.dart';
 import 'auth/forgot_password.dart';
-import './components/navbar.dart';
 import 'screens/home_screen.dart';
+import './components/navbar.dart';
 // import 'screens/library_screen.dart';
 import 'screens/community_screen.dart';
 import 'screens/settings_screen.dart';
@@ -13,34 +13,29 @@ import 'screens/settings_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
-void main() {
   runApp(const GameSellingApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class GameSellingApp extends StatelessWidget {
+  const GameSellingApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Firebase Login App',
-      theme: ThemeData.dark(),
-      debugShowCheckedModeBanner: false,
       title: 'Game Selling App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        brightness: Brightness.dark,
         primarySwatch: Colors.deepPurple,
         scaffoldBackgroundColor: const Color(0xFF0D0D0D),
       ),
       initialRoute: LoginScreen.routeName,
       routes: {
-        LoginScreen.routeName: (context) => const LoginScreen(),
-        HomeScreen.routeName: (context) => const HomeScreen(),
         LoginScreen.routeName: (_) => const LoginScreen(),
         RegisterScreen.routeName: (_) => const RegisterScreen(),
         ForgotPasswordScreen.routeName: (_) => const ForgotPasswordScreen(),
-        NavBar.routeName: (_) => const NavBar(),
         HomeScreen.routeName: (_) => const HomeScreen(),
+        NavBar.routeName: (_) => const NavBar(),
         // LibraryScreen.routeName: (_) => const LibraryScreen(),
         CommunityScreen.routeName: (_) => const CommunityScreen(),
         SettingsScreen.routeName: (_) => const SettingsScreen(),
